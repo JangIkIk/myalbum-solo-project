3a724d2e3fe485d8f582c537c0ee2e140b60f9e5
junik <wkdwnsdlr37@gmail.com>
2023-12-12 22:00:36 +0900

    nav디자인 위치 변경 (#5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,47 @@
-import type { Metadata } from 'next'
-import './globals.css'
-
-
+import type { Metadata } from "next";
+import "./globals.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export const metadata: Metadata = {
-  title: 'myalbum',
-  description: '나의 앨범',
-}
+  title: "myalbum",
+  description: "나의 앨범",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <div className="flex max-w-screen-xl m-auto h-screen">
+          <aside className="bg-layout-left w-[150px] flex flex-col items-center">
+            <div className="p-4">
+              <Link href={"/"}>
+                <Image
+                  src={"/assets/logo.png"}
+                  width={100}
+                  height={100}
+                  alt="logo"
+                  className="py-2.5"
+                />
+              </Link>
+            </div>
+            <nav>
+              <div className="router-button">
+                <Link href={"/login"}>로그인 | ID / PW 찾기</Link>
+              </div>
+              <div className="router-button">
+                <Link href={"/signup"}>회원가입</Link>
+              </div>
+            </nav>
+          </aside>
+          <main className="bg-layout-center flex-1">{children}</main>
+          <aside className="bg-layout-right w-[200px]">오른쪽 사이드바</aside>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
